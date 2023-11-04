1586bae91557e726ff5a514fdc2f9c6f06ad151b
Christofer Nolander <christofer@nolander.me>
2023-11-04 11:45:18 +0100

    swap raise and alt

--- keyboards/lily58/keymaps/nolanderc/keymap.c
+++ keyboards/lily58/keymaps/nolanderc/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,   KC_QUOT,   KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
             KC_ESC,   KC_A,      KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
             KC_LCTL,  KC_SCLN,   KC_Q,    KC_J,    KC_K,    KC_X, KC_MPLY,  KC_MPLY,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_BSLS,
-                               MO(_RAISE), KC_LALT, KC_LSFT,  MO(_LOWER),        KC_ENTER, KC_SPC, KC_RALT, MO(_RAISE)
+                               MO(_RAISE), KC_LALT, KC_LSFT,  MO(_LOWER),        KC_ENTER, KC_SPC, MO(_RAISE), KC_RALT
             ),
 
     [_QWERTY] = LAYOUT(
